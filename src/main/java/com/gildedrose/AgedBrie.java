1b1cb8abd24592a0c203120ac659dad4d7896591
Fuhua Zhang <zhangfuhua@hipagesgroup.com.au>
2020-03-02 23:42:51 +0800

    refactor: override isAgedBrie in AgedBrie class

--- src/main/java/com/gildedrose/AgedBrie.java
+++ src/main/java/com/gildedrose/AgedBrie.java
@@ -4,4 +4,9 @@ public class AgedBrie extends Item {
     public AgedBrie(int sell_in, int quality) {
         super("Aged Brie", sell_in, quality);
     }
+
+    @Override
+    public boolean isAgedBrie() {
+        return true;
+    }
 }
