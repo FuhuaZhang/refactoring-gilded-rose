854ce20e43c01d70515130aeedd35280f9a6b95e
Fuhua Zhang <zhangfuhua@hipagesgroup.com.au>
2020-03-02 23:26:19 +0800

    refactor: create AgedBrie extends Item

---
+++ src/main/java/com/gildedrose/Items/AgedBrie.java
@@ -0,0 +1,9 @@
+package com.gildedrose.Items;
+
+import com.gildedrose.Item;
+
+public class AgedBrie extends Item {
+    public AgedBrie(String name, int sell_in, int quality) {
+        super(name, sell_in, quality);
+    }
+}
